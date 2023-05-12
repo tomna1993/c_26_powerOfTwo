3252e5cfd0e388f4f946a779abee2e6194f85159
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 17:30:31 +0200

    Add powerOfTwo.c

---
+++ powerOfTwo.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Prompt user for a size and save it into a variable n.
+	// The number should be a positive integer number
+
+	// Create an array with n elements (n is the size which we asked from the user)
+
+	// Create a function to calculate each element of the array
+
+	// Print the array, integer by integer
+}
